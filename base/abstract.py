ae516438f79c9af459a24bc6faf05a28910dfdfa
Jan Scotka <jscotka@redhat.com>
2017-02-15 15:00:44 +0100

    added example config, what could be used for general testing of package

--- base/abstract.py
+++ base/abstract.py
@@ -2,25 +2,35 @@
 
 import os
 import re
+import yaml
+from avocado import Test
 from avocado import utils
+from avocado import main
 
 RHKEY = "fd431d51"
 
-class ContainerHelper():
+class CommonFunctions(Test):
+    """
+    :avocado: disable
+    """
+    def __init__(self, config):
+        
+
+class ContainerHelper(CommonFunctions):
     def __init__(self, icontainer):
         #brew-pulp-docker01.web.prod.ext.phx2.redhat.com:8888/rhel7/cockpit-ws:122-5
         #/mnt/redhat/brewroot/packages/cockpit-ws-docker/131/1/images/docker-image-sha256:71df4da82ff401d88e31604439b5ce67563e6bae7056f75f8f6dc715b64b4e02.x86_64.tar.gz
         self.tarbased=True
         self.name="testcontainer"
         self.icontainer = icontainer
-        self.prepareEnv()
-        self.startDocker()
-        self.pullImage()
+        self.prepare()
+        self.start()
+        self.pull()
 
-    def prepareEnv(self):
+    def prepare(self):
         process.run("yum -y install docker")
 
-    def startDocker(self):
+    def start(self):
         if not ".tar.gz" in self.icontainer:
             self.tarbased = False
             registry=re.search("([^/]*)", self.icontainer).groups()[0]
@@ -29,7 +39,7 @@ class ContainerHelper():
                     myfile.write("INSECURE_REGISTRY='--insecure-registry $REGISTRY %s'" % registry)
             process.run("system restart docker")
 
-    def pullImage(self):
+    def pull(self):
         if self.tarbased:
             process.run("docker import %s %s" % (self.icontainer, self.name))
         else:
@@ -37,27 +47,28 @@ class ContainerHelper():
             self.name = self.icontainer
     
 
-    def containerExec(self, args = "-t -i", command = "/bin/bash"):
+    def exec(self, args = "-it -d", command = "/bin/bash"):
         return process.run("docker run %s %s %s" % (args, self.name, command))
 
-    def containerStart(self, args = "-t -i", command = "/bin/bash"):
-        process.run("docker run %s %s %s" % (args, self.name, command))
-        process.run("docker ps | grep  %s" % self.name)
-        self.docker_id = process.run("docker ps | grep %s |cut -d " " -f 1" % self.name,shell=True).stdout
+    def run(self, args = "-it -d", command = "/bin/bash"):
+        self.docker_id = process.run("docker run %s %s %s" % (args, self.name, command)).stdout
 
-    def containerStop():
+
+    def stop():
         process.run("docker stop %s" % self.docker_id)
         process.run("docker rm %s" % self.docker_id)
 
-    def cockpitSigning():
+    def signing():
         # TODO not working because of "'
         if "bin" in self.containerExec(args="--entrypoint /bin/bash",command="ls /"): ...
         if "repolist: 0" in self.containerExec(args="--entrypoint /bin/bash",command="yum repolist"): ...
         packages=self.containerExec(args="--entrypoint /bin/bash",command='rpm -qa --qf="%{name}-%{version}-%{release} %{SIGPGP:pgpsig}\n"').stdout
 
-    def CockcpitLabelCheck(self,text):
+    def labels(self,text):
         if text in process.run("docker inspect %s"):...
 
+class dockerGeneral():
+    
 
 if __name__ == '__main__':
     main()
