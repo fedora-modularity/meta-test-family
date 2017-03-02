f08052fe1c558a37c48eae9c02040e4eb810fe68
Lukas Zachar <lzachar@redhat.com>
2017-03-02 19:02:31 +0100

    memcached: MODULE envar for behave

--- memcached/environment.py
+++ memcached/environment.py
@@ -1,5 +1,16 @@
 import moduleframework
 
+def get_correct_backend():
+    # FIXME this should shared IMHO
+    MODULE = moduleframework.MODULE if moduleframework.MODULE else "docker"
+    if MODULE == 'docker':
+        return moduleframework.ContainerHelper()
+    elif MODULE == 'rpm':
+        return moduleframework.RpmHelper()
+    else:
+        raise ValueError("Unsupported MODULE={0}".format(MODULE))
+
+
 def before_scenario(context, scenario):
     context.backend.setUp()
 
@@ -9,4 +20,4 @@ def after_scenario(context, scenario):
     context.backend.tearDown()
 
 def before_all(context):
-    context.backend = moduleframework.ContainerHelper()
+    context.backend = get_correct_backend()
