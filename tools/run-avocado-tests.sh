a004240dacbd0c56a8701e1b648823bf7d897ad6
Jan Scotka <jscotka@redhat.com>
2017-03-28 15:55:31 +0200

    typo, redefinig avocado function, it is bad bad bad

--- tools/run-avocado-tests.sh
+++ tools/run-avocado-tests.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-set -x
 PARAMS=$@
-AVOCADOCMD="avocado run -xunit"
+XUFILE="out.xunit"
+AVOCADOCMD="avocado run --xunit $XUFILE"
 
-function avocado(){
+function avocado_wrapper(){
     TESTS=`ls *.py *.sh`
     eval $PARAMS
     $AVOCADOCMD $TESTS
 }
 
-avocado
+avocado_wrapper
