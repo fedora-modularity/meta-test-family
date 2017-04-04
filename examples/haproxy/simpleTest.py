256d91ba9e2d9f696a2d78ab1002c17a63f7cb74
Jan Scotka <jscotka@redhat.com>
2017-04-04 15:26:39 +0200

    added some tweaks of haproxy tests

--- examples/haproxy/simpleTest.py
+++ examples/haproxy/simpleTest.py
@@ -34,8 +34,6 @@ class simpleTests(module_framework.AvocadoTest):
         self.runHost('docker pull docker.io/httpd')
         self.runHost('docker run --name http_name_8000 -d -p 8000:80 docker.io/httpd')
         self.runHost('docker run --name http_name_8001 -d -p 8001:80 docker.io/httpd')
-        global myip
-        myip = self.runHost('hostname -i').stdout.strip()
 
     def tearDown(self):
         super(self.__class__, self).tearDown()
@@ -45,7 +43,6 @@ class simpleTests(module_framework.AvocadoTest):
         self.runHost('docker rm http_name_8001')
 
     def testAssertIn(self):
-        self.runHost(('sed s/127.0.0.1/{}/ my-haproxy.cfg > local-haproxy.cfg').format(myip), shell = True)
         self.start()
         self.assertIn('It works!',self.runHost('curl localhost:8077', shell = True).stdout)
 
