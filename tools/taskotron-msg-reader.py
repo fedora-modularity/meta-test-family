598f84b6f3a1b5f8b4c88f5f737e130d6cfffb72
Jan Scotka <jscotka@redhat.com>
2017-04-11 12:36:51 +0200

    typo in fedmsg parser

--- tools/taskotron-msg-reader.py
+++ tools/taskotron-msg-reader.py
@@ -76,7 +76,7 @@ if options.filename:
     print " ".join(a.generateParams())
 elif options.release:
     a = FedMsgParser(options.release,taskotron=True)
-    print " ".join(a.out)
+    print " ".join(a.generateParams())
 else:
     raise Exception(parser.print_help())
 
