9a1af63d5148aa493ef64a252a3187398f27a07c
Jan Scotka <jscotka@redhat.com>
2017-04-12 14:36:44 +0200

    there were typo in koji downloader

--- moduleframework/pdc_data.py
+++ moduleframework/pdc_data.py
@@ -80,7 +80,7 @@ class PDCParser():
         absdir = os.path.abspath(dirname)
         for foo in utils.process.run("koji list-tagged --quiet %s" % self.pdcdata["koji_tag"]).stdout.split("\n"):
             pkgbouid = foo.strip().split(" ")[0]
-            utils.process.run("cd %s; do koji download-build %s" % (absdir, pkgbouid), shell=True)
+            utils.process.run("cd %s; koji download-build %s" % (absdir, pkgbouid), shell=True)
         utils.process.run("cd %s; createrepo -v %s" % (absdir,absdir), shell=True)
         return "file://%s" % dirname
 
