a8624101873a25ef0bea274613e02f8564c497f1
Jan Scotka <jscotka@redhat.com>
2017-05-12 09:22:47 +0200

    version read from specfile, test if it will work well

--- moduleframework/version.py
+++ moduleframework/version.py
@@ -1,1 +1,24 @@
-VERSION = '0.4.19'
+#!/usr/bin/python
+
+import os
+
+SPECFILEPATH = os.path.abspath(
+    os.path.join(
+        os.path.dirname(
+            os.path.dirname(__file__)
+        ),
+        "modularity-testing-framework.spec"
+    ))
+
+
+def version_func():
+    with open(SPECFILEPATH, 'r') as infile:
+        for line in infile.readlines():
+            if "Version:        " in line:
+                return line[16:].strip()
+    raise BaseException("Unable to read Version string from specfile:", SPECFILEPATH)
+
+VERSION=version_func()
+
+if __name__ == '__main__':
+    print VERSION
