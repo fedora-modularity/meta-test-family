dc2a59d4a97a02513db221c58f9c09a0d0de2f5d
Petr "Stone" Hracek <phracek@redhat.com>
2017-05-29 09:18:54 +0200

    Templates for test developers
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

---
+++ examples/template/sanity_template.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# This Modularity Testing Framework helps you to write tests for modules
+# Copyright (C) 2017 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# he Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Authors: Jan Scotka <jscotka@redhat.com>
+#
+
+import socket
+from avocado import main
+from avocado.core import exceptions
+from moduleframework import module_framework
+
+
+class SanityCheckTemplate(module_framework.AvocadoTest):
+    """
+    :avocado: enable
+    """
+
+    def testFirst(self):
+        self.start()
+        # Write your first test
+
+    def testSecond(self):
+        self.start()
+        # Write your second test
+
+
+if __name__ == '__main__':
+    main()
