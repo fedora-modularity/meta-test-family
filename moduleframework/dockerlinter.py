8a7b7f4179871efa58e1c48b4a89b2cc909f721a
Petr "Stone" Hracek <phracek@redhat.com>
2017-07-24 12:22:09 +0200

    Use dnf instead of microdnf
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- moduleframework/dockerlinter.py
+++ moduleframework/dockerlinter.py
@@ -173,18 +173,16 @@ class DockerfileLinter(object):
         if FROM in self.docker_dict:
             return [x for x in self.docker_dict[FROM] if "baseruntime/baseruntime" in x]
 
-    def check_microdnf(self):
+    def check_dnf(self):
         """
         Function returns docker labels
         :return: label dictionary
         """
         if RUN in self.docker_dict:
             for val in self.docker_dict[RUN]:
-                if val.startswith("yum") or " yum " in val:
-                    return False
                 if val.startswith("dnf") or " dnf " in val:
-                    return False
-                else:
                     return True
+                else:
+                    return False
 
 
