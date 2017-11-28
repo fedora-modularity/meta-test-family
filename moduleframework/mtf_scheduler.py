491e532eb5c368ed0a7eea98cc8e8e924ff2be8e
Petr Sklenar <psklenar@redhat.com>
2017-11-28 13:44:22 +0100

    add all variables

--- moduleframework/mtf_scheduler.py
+++ moduleframework/mtf_scheduler.py
@@ -38,17 +38,38 @@ def mtfparser():
 """
 VARIABLES
 
-       CONFIG defines the module configuration file. It defaults to config.yaml
+    AVOCADO_LOG_DEBUG=yes enables avocado debug output.
 
-       MODULE defines tested module type, if default-module is not set in config.yaml.
+    DEBUG=yes enables debugging mode to test output.
 
-       URL to container.
-              E.g. URL=docker.io/modularitycontainers/haproxy if MODULE=docker
+    CONFIG defines the module configuration file. It defaults to config.yaml.
 
-       MODULEMDURL
-              overwrites the location of a moduleMD file.
+    MODULE defines tested module type, if default-module is not set in config.yaml.
 
-       and more at http://meta-test-family.readthedocs.io/en/latest/user_guide/environment_variables.html
+            =docker uses the docker section of config.yaml.
+            =rpm uses the rpm section of config.yaml and tests RPMs directly on a host.
+            =nspawn tests RPMs in a virtual environment with systemd-nspawn.
+
+    URL overrides the value of module.docker.container or module.rpm.repo.
+       The URL should correspond to the MODULE variable, for example:
+            URL=docker.io/modularitycontainers/haproxy if MODULE=docker
+            URL=https://phracek.fedorapeople.org/haproxy-module-repo # if MODULE=nspawn or MODULE=rpm
+
+    MODULEMDURL overwrites the location of a moduleMD file.
+
+    COMPOSEURL overwrites the location of a compose Pungi build.
+
+    MTF_SKIP_DISABLING_SELINUX=yes
+       does not disable SELinux. In nspawn type on Fedora 25 SELinux should be disabled,
+       because it does not work well with SELinux enabled.
+
+    MTF_DO_NOT_CLEANUP=yes does not clean up module after tests execution.
+
+    MTF_REUSE=yes uses the same module between tests. It speeds up test execution.
+
+    MTF_REMOTE_REPOS=yes disables downloading of Koji packages and creating a local repo.
+
+    MTF_DISABLE_MODULE=yes disables module handling to use nonmodular test mode.
 """
     parser = argparse.ArgumentParser(
         # TODO
