1fd6cb23d75ada47490a45ab7a0bfc11545cf6b5
jscotka <scottyh@post.cz>
2017-12-01 12:48:15 +0100

    Revert "remove all manpages"

--- setup.py
+++ setup.py
@@ -23,11 +23,18 @@
 import os
 import sys
 
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
+from setuptools.command.build_py import build_py
+from setuptools.command.install import install
 
+try:
+    sys.path = [os.path.join(os.getcwd(), 'build_manpages')] + sys.path
+    from build_manpages.build_manpages import build_manpages, get_build_py_cmd, get_install_cmd
+except:
+    print("=======================================")
+    print("Use 'git submodule update --init' first")
+    print("=======================================")
+    raise
 
 # copy from https://github.com/avocado-framework/avocado/blob/master/setup.py
 VIRTUAL_ENV = hasattr(sys, 'real_prefix')
@@ -106,5 +113,10 @@ setup(
         'Programming Language :: Python',
         'Topic :: Software Development',
     ],
-    install_requires=open('requirements.txt').read().splitlines()
+    install_requires=open('requirements.txt').read().splitlines(),
+    cmdclass={
+        'build_manpages': build_manpages,
+        'build_py': get_build_py_cmd(build_py),
+        'install': get_install_cmd(install),
+    },
 )
