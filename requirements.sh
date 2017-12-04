ec092bb10bcdbd9ec21031e0a03d1a4a9b974f0e
Jan Scotka <jscotka@redhat.com>
2017-12-04 09:32:44 +0100

    dependencies for testing in nicer and cleaner format for various distros

--- requirements.sh
+++ requirements.sh
@@ -1,11 +1,45 @@
 #!/bin/bash
 
-GENERICPACKAGES="make git curl"
-RPMPACKAGES="httpd python2-avocado python2-avocado-plugins-output-html
-             python-netifaces redhat-rpm-config python2-devel python-gssapi krb5-devel"
-APTPACKAGES="python-software-properties software-properties-common python-pip python-dev
-             build-essential libkrb5-dev netcat mysql-client-5.5 python-pytest"
-PIPPACKAGES="avocado-framework"
+GENERICPACKAGES="
+curl
+git
+make
+python-pip
+"
+RPMPACKAGES="
+fedpkg
+httpd
+koji
+krb5-devel
+nc
+pdc-client
+python-devel
+python-gssapi
+python-netifaces
+python2-avocado
+python2-avocado-plugins-output-html
+python2-devel
+python2-dockerfile-parse
+python2-modulemd
+python2-odcs-client
+python2-pytest
+redhat-rpm-config
+"
+
+APTPACKAGES="
+build-essential
+libkrb5-dev
+mysql-client-5.5
+netcat
+python-dev
+python-pytest
+python-software-properties
+software-properties-common
+"
+
+PIPPACKAGES="
+avocado-framework
+"
 
 if [ -e /usr/bin/dnf ]; then
     dnf -y install $GENERICPACKAGES $RPMPACKAGES
