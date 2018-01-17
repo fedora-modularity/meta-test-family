2711a67145d544cd23407cd0d6acb59a0541385e
Petr "Stone" Hracek <phracek@redhat.com>
2018-01-17 16:18:45 +0100

    Automatic commit of package [meta-test-family] release [0.7.10-1].
    Created by command:
    
    /usr/bin/tito tag

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ for path in paths:
 
 setup(
     name='meta-test-family',
-    version="0.7.9",
+    version="0.7.10",
     description='Tool to test components for a modular Fedora.',
     keywords='modules,containers,testing,framework',
     author='Jan Scotka',
